829c45a80a64401a6969c42f00f72333062ac850
Ckohwo <charleskohwo@gmail.com>
2025-10-23 20:18:16 +0100

    little update by importing os module

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import threading
+import os
 from level3_automated_hybrid_ingestion.automated_scraper import run_ingestion_cycle, commit_data_to_git
 
 app = Flask(__name__)
