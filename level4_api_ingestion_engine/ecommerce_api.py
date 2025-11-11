3ca667e07090db3a5f41f2f2d13d2bee3c8486d3
Ckohwo <charleskohwo@gmail.com>
2025-11-11 20:26:07 +0100

    updated the api_ingestion to actual eccommerce api

--- level4_api_ingestion_engine/ecommerce_api.py
+++ level4_api_ingestion_engine/ecommerce_api.py
@@ -6,28 +6,73 @@ import time
 from datetime import datetime
 
 
-URL = "https://dummyjson.com/products"
+API_URL = "https://api.konga.com/v1/graphql"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64), AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"}
+
 
 CSV_PATH = Path(__file__).resolve().parents[1] / "data" / "ecommerce_api_dataset.csv"
 
-  
-# Function to safely get data from API with retries 
+# GraphQL query payload
+payload = {
+  "query": """
+    {
+      searchByStore(
+        search_term: [["category.category_id:5237"]],
+        numericFilters: [],
+        sortBy: "",
+        paginate: {page: 0, limit: 40},
+        store_id: 1
+      ) {
+        pagination {
+          limit
+          page
+          total
+        }
+        products {
+          name
+          brand
+          price
+          deal_price
+          final_price
+          description
+          image_thumbnail
+          sku
+          seller {
+            name
+          }
+          stock {
+            in_stock
+            quantity
+          }
+          product_rating {
+            quality {
+              average
+              number_of_ratings
+            }
+          }
+        }
+      }
+    }
+  """
+}
 
-def safe_get(URL, retries = 4, delay = 4): 
+
+  
+# Function to safely get data from API with retries  
+def safe_get(API_URL, retries = 4, delay = 4): 
     #retries--The maximum number of attempts the function will make.
     #delay--The number of seconds the script will pause before making the next attempt.
     
     for attempt in range(retries):
         try:
-            print(f"🌐 Attemp=t {attempt}: Fetching data from {URL}") 
-            response = requests.get(URL, timeout = 10, verify=True)
+            print(f"🌐 Attemp=t {attempt}: Fetching data from {API_URL}") 
+            response = requests.post(API_URL,headers = headers, json=payload, timeout = 10, verify=True)
 
-            if response.status_code == 200:
-                print("All is well...data fetched successfully!")
-                return response.json() # Return the JSON data as a Python dictionary
+            response.raise_for_status()
+            print("All is well...data fetched successfully!")
+            return response.json() # Return the JSON data as a Python dictionary
             
-            else:
-                print(f"⚠️ Received status code {response.status_code}, retrying...")
+            
 
         except requests.exceptions.RequestException as e:
             print(f"❌ Error: {e}. Retrying in {delay}s...")
@@ -39,70 +84,39 @@ def safe_get(URL, retries = 4, delay = 4):
     return None
 
 
-# Function to normalize raw product data into a clean DataFrame 
 
-def normalize_products(data):
+# Function to normalize raw product data into a clean DataFrame  
+def load(data):
      
     if not data:
         print("⚠️ No data to normalize.")
         return pd.DataFrame()  # Return empty DataFrame
-    
-    records = []
-    for item in data.get("products", []):
-        try:
-            record = {
-                "ProductID": item.get("id"),
-                "Title": item.get("title"),
-                "Description": item.get("description"), 
-                "Category": item.get("category"),
-                "Price": item.get("price"),
-                "discountPercentage": item.get("discountPercentage"),
-                "Rating": item.get("rating"),
-                "Stock": item.get("stock"),
-                "Tags": ", ".join(item.get("tags", [])),
-                "Brand": item.get("brand"),
-                "SKU": item.get("sku"),
-                "weight": item.get("weight"),
-                "Dimensions": item.get("dimensions"),
-                "warranty": item.get("warrantyInformation"),
-                "Shiiping_Info": item.get("shippingInformation"),
-                "Availability": item.get("availabilityStatus"),
-                "Reviews": item.get("reviews"),
-                "ReturnPolicy": item.get("returnPolicy"),
-                "Meta_Keywords": item.get("meta"),
-                "Barcode": item.get("barcode"),
-                "qrCode": item.get("qrCode"),
-                "ImageURL": item.get("image"),
-                "ThumbnailURL": item.get("thumbnail"),
-                "Timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-                "Source": "dummyjson.com"
-            }
-            records.append(record)
-        except Exception as e:
-            print(f"❌ Error normalizing record: {e}")
-            continue
 
-    df = pd.DataFrame(records)
-    df['fetched_at'] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"✅ Normalized {len(df)} products into a clean DataFrame.")
-    return df
+    # Extract products 
+    df = json.dumps(data, indent=4)
+    products = data["data"]["searchByStore"]["products"]
+    dfs = pd.DataFrame(products)
+    dfs['fetched_at'] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"✅ Normalized {len(dfs)} products into a clean DataFrame.")
+    return dfs
 
 
-def save_to_csv(df, CSV_PATH):
+# Function to save DataFrame to CSV, appending if file exists
+def save_to_csv(dfs, CSV_PATH):
     try:
         CSV_PATH.parent.mkdir(parents=True, exist_ok=True)
 
 
         if CSV_PATH.exists():
             existing_df = pd.read_csv(CSV_PATH)
-            combined_df = pd.concat([existing_df, df], ignore_index=True)
+            combined_df = pd.concat([existing_df, dfs], ignore_index=True)
          # The Procudt id is used for mapping individual iitems, we can 
          # drop duplicates based on ProductID (if it exists)
              
             combined_df.to_csv(CSV_PATH, index=False)
             print(f"💾 Data appended & saved successfully to {CSV_PATH}")
         else:
-            df.to_csv(CSV_PATH, index=False)
+            dfs.to_csv(CSV_PATH, index=False)
             print(f"💾 New CSV created at {CSV_PATH}")
 
     except Exception as e:
@@ -111,8 +125,8 @@ def save_to_csv(df, CSV_PATH):
      
 
 if __name__ == "__main__":
-    data = safe_get(URL)
-    df = normalize_products(data) 
-    if not df.empty:
-        save_to_csv(df, CSV_PATH)
+    data = safe_get(API_URL)
+    dfs = load(data) 
+    if not dfs.empty:
+        save_to_csv(dfs, CSV_PATH)
      
